b0ad97a18a8210b65621ec502f1271f6f688ed3d
ajaykumar133 <53463124+ajaykumar133@users.noreply.github.com>
2019-12-11 09:01:34 +0530

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -1,34 +1,26 @@
+#=============== to check if a year is leap year or not  ============================#
 
-##--------------------- to get calender of any month of any year ---------------------#
-#import calendar
+year = int(input("enter the year you want to check :"))
 
-#yy=1998
-#mm=10
+print("year = ",year)
 
-#print(calendar.month(yy,mm))
+if (year%4==0) :
+    print("Yes, year",year,"it is a leap year")
+else:
+    print(year," is not leap year ")
 
-###########TO CHECK IS A NO. IS PRIME NO. OR NOT ####################
-
-#year = int(input("enter the year you want to check :"))
-
-#print("year = ",year)
-
-#if (year%4==0) :
- #   print("Yes, year",year,"it is a leap year")
-#else:
- #   print(year," is not leap year ")
-
-#################  TO CHECK IF ELEMENT EXIST IN THE GIVEN LIST ################
+# ============== TO CHECK IF ELEMENT EXIST IN THE GIVEN LIST ========================#
 
 
-#list=['hlo','ye','gh','ab']
+list=['hlo','ye','gh','ab']
 
-#if 'ab' in list:
- #   print("\n\n yes the element exist in list .")
-#else:
- #   print("no the element exist in list .")
+if 'ab' in list:
+    print("\n\n yes the element exist in list .")
+else:
+    print("no the element exist in list .")
 
-############# str concatination
+#=============== string concatination ============================================#
+
 str1= input("enter 1st str : ")
 str2= input("enter 2nd str : ")
 strctn=str1+str2
