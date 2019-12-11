27c740e4e0be5a9335e8bc705b06df20b05ddfff
ajaykumar133 <53463124+ajaykumar133@users.noreply.github.com>
2019-12-11 08:55:39 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,19 +1,18 @@
 
 arr = []
 
-num = int(input("Enter the size of array : "))
+num = int(input("Enter the size of array : ")) 
 
 for n in range (num):
-    numbers = int(input("Enter the no. in array : "))
+    numbers = int(input("Input the no. in array : "))
     arr.append(numbers)
 print ("Array = ",arr)
 
 print("\nThe max no. = ", max(arr))
 print("The min no. = ", min(arr))
 
+
 # for already given array........
-#ar = [10,35,56,65,345]
-#print("in array2 : ",max(ar))
 
-# forloop , if-else, inheri, ovrride, str concatination, searh&sort , exception , mthod, array.
-
+array2 = [10,35,56,65,345]
+print("in array2 max no. is : ",max(ar))  # using in-bulit method max()
